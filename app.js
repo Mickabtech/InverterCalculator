870d4094f4ea3026b15c23cabdff5cdc42353c2a
agboola <mickabtech@gmail.com>
2023-05-18 12:18:11 +0100

    modal

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 
 const btn = document.getElementById("submit")
+const modal  = document.getElementById("modal");
+const closeModal = document.querySelector("span")
+
 
 btn.addEventListener("click", (e) => {
-
     const inpt1 = document.getElementById("inpt1").value
     const inpt2 = document.getElementById("inpt2").value
+    const words = document.getElementById("words")
+    const words1 = document.getElementById("words1")
+    const words2 = document.getElementById("words2")
+    const words3 = document.getElementById("words3")
+    modal.style.display = "flex"
 
     const feel = inpt1 / 0.8
     const beal = inpt2 / 1200
     const ceal = inpt2 / 2400
     const deal = inpt2 / 2640
     
-    console.log(feel + " " + "watt Inverter")
-    console.log("For a 12v 100ah batteries,"+" " + Math.ceil(beal) + " " + "Batteries")
-    console.log("For a 12v 200ah batteries,"+" " + Math.ceil(ceal) + " " + "Batteries")
-    console.log("For a 12v 220ah batteries,"+" " + Math.ceil(deal) + " " + "Batteries")
+    words.textContent = feel + " " + "watt Inverter" 
+    words1.textContent = "For a 12v 100ah batteries,"+" " + Math.ceil(beal) + " " + "Batteries"
+    words2.textContent = "For a 12v 200ah batteries,"+" " + Math.ceil(ceal) + " " + "Batteries" 
+    words3.textContent = "For a 12v 220ah batteries,"+" " + Math.ceil(deal) + " " + "Batteries"
     e.preventDefault()
 });
 
+
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none"
+    err.textContent = ""
+})
+
+window.onclick = (e) =>{
+    if (e.target.className === "myModal"){
+        modal.style.display = "none"
+        err.textContent = ""
+    }
+}
